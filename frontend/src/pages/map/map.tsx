088599573119b2f5569bdf571a19865765591992
Grigory Vorobev <grishaMas@yandex.ru>
2025-05-13 17:40:29 +0500

    FIX: Dependency conflict

--- frontend/src/pages/map/map.tsx
+++ frontend/src/pages/map/map.tsx
@@ -1,42 +1,113 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
-// Стили для контейнера карты
-const mapStyles = {
-  height: '100vh',
-  width: '100%',
-};
+import { GeoJSONProps, MapContainer, Marker, Popup, GeoJSON, TileLayer } from 'react-leaflet';
 
 const MapComponent: React.FC = () => {
-    const Bounds = [[-90, -180], [90, 180]] as L.LatLngBoundsLiteral
-    const maxBounds = [[-98, -180], [90, 180]] as L.LatLngBoundsLiteral;
+    const Geo_json: GeoJSON.FeatureCollection = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "coordinates": [
+              [
+                [
+                  -65.62695249312675,
+                  -54.77912422989706
+                ],
+                [
+                  -65.62695249312675,
+                  -47.50828093599764
+                ],
+                [
+                  -63.04443700760703,
+                  -40.656077518500865
+                ],
+                [
+                  -51.42311732276852,
+                  -33.020707212332816
+                ],
+                [
+                  -40.318300735033716,
+                  -21.335116867739515
+                ],
+                [
+                  -34.636766666890566,
+                  -6.851001426380648
+                ],
+                [
+                  -52.972626614080454,
+                  4.491076657642324
+                ],
+                [
+                  -66.14345559023086,
+                  10.37824702116498
+                ],
+                [
+                  -76.73176972211766,
+                  10.378245970942984
+                ],
+                [
+                  -80.08903985329337,
+                  -2.992252218169014
+                ],
+                [
+                  -74.14925438360207,
+                  -12.449779792162857
+                ],
+                [
+                  -70.79198425242637,
+                  -19.15460945542361
+                ],
+                [
+                  -70.79198413098823,
+                  -29.93793909837097
+                ],
+                [
+                  -73.89100258014943,
+                  -41.32019013930618
+                ],
+                [
+                  -74.66575722580542,
+                  -50.947997533843356
+                ],
+                [
+                  -73.37449948304575,
+                  -54.39104267669955
+                ],
+                [
+                  -65.62695249312675,
+                  -54.77912422989706
+                ]
+              ]
+            ],
+            "type": "Polygon"
+          }
+        }
+      ]
+    }
 
-    const geoportailUrl =
-    'https://data.geopf.fr/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}';
-    const geoportailOptions = {
-    minZoom: 2.5,
-    maxZoom: 6,
-    bounds: Bounds,
-    format: 'image/jpeg',
-    style: 'normal',
-    };
+    return(
+        <>
+        <MapContainer 
+        center={[0,0]}
+        zoom={2}
+        style={{height: "100vh", width: '100%'}}
+        maxZoom={10}
+        minZoom={2}
+        >
+        <TileLayer
+          url="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution="© ArcGIS © Esri"
+        />
 
-    return (
-    <MapContainer
-        center={[0, 0]} // Центр карты, например, Париж
-        zoom={2.5}
-        style={mapStyles}
-        maxBounds={maxBounds}
-        maxBoundsViscosity={1.0} // Упругость границ (1.0 — жесткие границы)
-    >
-    <TileLayer
-    url={geoportailUrl}
-    {...geoportailOptions}
-    noWrap={true}
-    />
-    </MapContainer>
-    );
-};
+        <Marker position={[0,0]}>
+            <Popup>ЭЙ ЧЕБУРЕК</Popup>
+        </Marker>
+
+        <GeoJSON data={Geo_json}/>
+        </MapContainer>
+        </>
+    )
+}
 
 export default MapComponent;
