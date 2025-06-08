b9f1904c9926dc14532e609c9fe7e199e2a0a942
Grigory Vorobev <grishaMas@yandex.ru>
2025-06-08 18:17:17 +0500

    MODIFED: Added routing to header.

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,13 @@
 import '../assets/styles/header.css'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+    const navigate = useNavigate()
+
     return (
         <header>
             <div className='header_container'>
-                <div className='logo image'/>
+                <div className='logo image' onClick={() => navigate('/')}/>
                 <div className='catalog image' />
             </div>
         </header>
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,9 +1,11 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from '../pages/home'
 import MapPage from '../pages/map'
+import Header from '../components/header'
 
 function Router() {
     return <BrowserRouter>
+    <Header/>
         <Routes>
             <Route element={<Home />} path='/' />
             <Route element={<MapPage />} path='/map' />
