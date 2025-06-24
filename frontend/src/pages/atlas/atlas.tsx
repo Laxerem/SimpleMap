a6a8b409946156d557e03be11f96310eba61feaa
Grigory Vorobev <grishaMas@yandex.ru>
2025-06-24 18:39:41 +0500

    MODIFED: Delete an useless trash.

--- frontend/src/pages/atlas/atlas.tsx
+++ frontend/src/pages/atlas/atlas.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, ReactNode } from "react"
+import React, { PropsWithChildren } from "react"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'; // основные стили
 import 'swiper/css/navigation'; // если нужен navigation
